ba9eb227eb8ec03e04f4f928d5faf008783dadeb
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/CustomGridControl/MyGridRegistration.cs
@@ -0,0 +1,18 @@
+﻿using DevExpress.XtraGrid;
+using DevExpress.XtraGrid.Registrator;
+using DevExpress.XtraGrid.Views.Base;
+using DevExpress.XtraGrid.Views.Base.Handler;
+using DevExpress.XtraGrid.Views.Base.ViewInfo;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace T263441 {
+    public class MyGridViewInfoRegistrator : GridInfoRegistrator {
+        public override string ViewName { get { return "MyGridView"; } }
+        public override BaseView CreateView(GridControl grid) { return new MyGridView(grid as GridControl); }
+        public override BaseViewInfo CreateViewInfo(BaseView view) { return new MyGridViewInfo(view as MyGridView); }
+    }
+}
---
+++ VB/Customer.vb
@@ -0,0 +1,72 @@
+﻿Imports System
+Imports System.Collections.Generic
+Imports System.Linq
+Imports System.Text
+Imports System.Threading.Tasks
+
+Namespace T263441
+    Public Class Customer
+        Private _firstName As String
+        Private _lastName As String
+        Private _country As String
+        Private _hasChildren As Boolean
+        Private _cash As Integer
+
+        Public Sub New()
+        End Sub
+
+        Public Property FirstName() As String
+            Get
+                Return _firstName
+            End Get
+            Set(ByVal value As String)
+                If value <> _firstName Then
+                    _firstName = value
+                End If
+            End Set
+        End Property
+        Public Property LastName() As String
+            Get
+                Return _lastName
+            End Get
+            Set(ByVal value As String)
+                If value <> _lastName Then
+                    _lastName = value
+                End If
+            End Set
+        End Property
+
+        Public Property Country() As String
+            Get
+                Return _country
+            End Get
+            Set(ByVal value As String)
+                If value <> _country Then
+                    _country = value
+                End If
+            End Set
+        End Property
+
+        Public Property HasChildren() As Boolean
+            Get
+                Return _hasChildren
+            End Get
+            Set(ByVal value As Boolean)
+                If value <> _hasChildren Then
+                    _hasChildren = value
+                End If
+            End Set
+        End Property
+
+        Public Property Cash() As Integer
+            Get
+                Return _cash
+            End Get
+            Set(ByVal value As Integer)
+                If value <> _cash Then
+                    _cash = value
+                End If
+            End Set
+        End Property
+    End Class
+End Namespace
